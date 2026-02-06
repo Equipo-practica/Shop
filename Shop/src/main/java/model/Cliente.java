1af7b99c74e79ff3aa7c3004b61ceb44443d7060
i0rph3ni <iorpheni@gmail.com>
2026-02-06 13:29:19 +0100

    Clase Cliente.java creada y añadida al push

--- Shop/src/main/java/model/Cliente.java
+++ Shop/src/main/java/model/Cliente.java
@@ -1,4 +1,53 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Cliente {
+
+    private int idCliente;      // Corresponde a id_cliente
+    private String nombre;
+    private String email;
+    private boolean vip;        // Corresponde a VIP (boolean)
+    private LocalDate fechaAlta;// Corresponde a fecha_alta
+
+    public Cliente() {
+    }
+
+    // Constructor SIN ID (para Insertar)
+    public Cliente(String nombre, String cliente, String email, boolean vip, LocalDate fechaAlta) {
+        this.nombre = nombre;
+        this.email = email;
+        this.vip = vip;
+        this.fechaAlta = fechaAlta;
+    }
+
+    // Constructor CON ID (para Consultar/Leer de BD)
+    public Cliente(int idCliente, String nombre, String cliente, String email, boolean vip, LocalDate fechaAlta) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.email = email;
+        this.vip = vip;
+        this.fechaAlta = fechaAlta;
+    }
+
+    // Getters y Setters
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public boolean isVip() { return vip; } // El getter de boolean suele empezar por 'is'
+    public void setVip(boolean vip) { this.vip = vip; }
+
+    public LocalDate getFechaAlta() { return fechaAlta; }
+    public void setFechaAlta(LocalDate fechaAlta) { this.fechaAlta = fechaAlta; }
+
+    @Override
+    public String toString() {
+        return "Cliente [id=" + idCliente + ", nombre=" + nombre + ", vip=" + vip + "]";
+    }
 }
